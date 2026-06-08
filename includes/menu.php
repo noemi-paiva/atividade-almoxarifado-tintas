0d0461d5058e9aee3d391842e4c9d398a8292407
noemi-paiva <noemipaivasantos@gmail.com>
2026-06-08 11:32:58 -0300

    Create menu.php
    Página menu.

---
+++ includes/menu.php
@@ -0,0 +1,10 @@
+<nav>
+    <ul>
+        <li><a href="/atividade-almoxarifado-tintas/index.php">Início</a></li>
+        <li><a href="/atividade-almoxarifado-tintas/pages/produtos/listar.php">Produtos</a></li>
+        <li><a href="/atividade-almoxarifado-tintas/pages/movimentacoes/listar.php">Movimentações</a></li>
+        <li><a href="/atividade-almoxarifado-tintas/login.php">Login</a></li>
+    </ul>
+</nav>
+
+<hr>
