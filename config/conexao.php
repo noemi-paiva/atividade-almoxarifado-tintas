2e5f7168311efb2e8d847b88feb77e06e03cbc95
noemi-paiva <noemipaivasantos@gmail.com>
2026-06-08 11:30:00 -0300

    Update conexao.php
    Código inserido.

--- config/conexao.php
+++ config/conexao.php
@@ -1,1 +1,18 @@
+<?php
 
+$host = "localhost";
+$usuario = "root";
+$senha = "";
+$banco = "almoxarifado_tintas";
+
+$conexao = mysqli_connect(
+    $host,
+    $usuario,
+    $senha,
+    $banco
+);
+
+if (!$conexao) {
+    die("Erro na conexão com o banco de dados.");
+}
+?>
