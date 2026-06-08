66fa2a9a10fbc7c42a72e58fa7956fd49cbaaf29
noemi-paiva <noemipaivasantos@gmail.com>
2026-06-08 11:37:23 -0300

    Update menu.php
    Alteração do menu.

--- includes/menu.php
+++ includes/menu.php
@@ -1,9 +1,8 @@
 <nav>
     <ul>
         <li><a href="/atividade-almoxarifado-tintas/index.php">Início</a></li>
-        <li><a href="/atividade-almoxarifado-tintas/pages/produtos/listar.php">Produtos</a></li>
-        <li><a href="/atividade-almoxarifado-tintas/pages/movimentacoes/listar.php">Movimentações</a></li>
-        <li><a href="/atividade-almoxarifado-tintas/login.php">Login</a></li>
+        <li><a href="#">Produtos</a></li>
+        <li><a href="#">Movimentações</a></li>
     </ul>
 </nav>
 
